682bf05b429568577f7cff3a9d6d5285393d41c3
Shikeless <Shikeless@gmail.com>
2019-10-27 13:58:25 +0300

    building project

---
+++ build/precache-manifest.ce8fc6835ab61c02cc9c9bb850c49745.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ff48e673421e5f38ec0c44b26d9eed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b77c7723020eec15195",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "da99f1858670231d3f96",
+    "url": "/static/css/main.9b706cb1.chunk.css"
+  },
+  {
+    "revision": "0b77c7723020eec15195",
+    "url": "/static/js/2.8c5c9a8b.chunk.js"
+  },
+  {
+    "revision": "da99f1858670231d3f96",
+    "url": "/static/js/main.4effdca6.chunk.js"
+  },
+  {
+    "revision": "2bef53f2256e337b2c8a",
+    "url": "/static/js/runtime~main.804de7ef.js"
+  }
+]);
